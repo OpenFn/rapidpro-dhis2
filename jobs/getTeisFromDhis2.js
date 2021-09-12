77e26aae3c24ff2d78e5b23b4d04daa8844ad446
Taylor Downs <downs.taylor@gmail.com>
2021-09-12 16:58:25 +0300

    prettier

--- jobs/getTeisFromDhis2.js
+++ jobs/getTeisFromDhis2.js
@@ -1,12 +1,19 @@
-getTEIs({
-  fields: '*',
-  ou: 'DiszpKrYNg8',
-  program: 'uy2gU8kT1jF',
-  //trackedEntityInstance: 'jQtdJHakJdj',
-  skipPaging: true,
-}, {}, state => {
-  const {data} = state;
-  console.log(data.trackedEntityInstances.map(tei => tei.attributes.map(a => `${a.displayName}: ${a.value}`))) //Log DHIS2 response
-  return state;
-});
-
+getTEIs(
+  {
+    fields: '*',
+    ou: 'DiszpKrYNg8',
+    program: 'uy2gU8kT1jF',
+    //trackedEntityInstance: 'jQtdJHakJdj',
+    skipPaging: true,
+  },
+  {},
+  state => {
+    const { data } = state;
+    console.log(
+      data.trackedEntityInstances.map(tei =>
+        tei.attributes.map(a => `${a.displayName}: ${a.value}`)
+      )
+    ); //Log DHIS2 response
+    return state;
+  }
+);
