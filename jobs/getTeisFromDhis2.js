24a97688f06ad5000452694d7b33a45ccffaf262
Aleksa Krolls <aleksagk@gmail.com>
2021-09-12 16:52:42 +0300

    aleksa@openfn.org's change on OpenFn.org

--- jobs/getTeisFromDhis2.js
+++ jobs/getTeisFromDhis2.js
@@ -5,7 +5,7 @@ getTEIs({
   //trackedEntityInstance: 'jQtdJHakJdj',
   skipPaging: true,
 }, {}, state => {
-  console.log(state.data); //Log DHIS2 response
+  console.log(state.data.trackedEntityInstances.map(x => JSON.stringify(x.attributes, null, 2))); //Log DHIS2 response
   return state;
 });
 
