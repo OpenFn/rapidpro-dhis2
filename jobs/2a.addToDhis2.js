72d249bdeeeaf6ca683ca08f95cece07004fa1a7
Taylor Downs <taylor@openfn.org>
2021-09-15 20:50:01 +0100

    taylor@openfn.org's change on OpenFn.org

--- jobs/2a.addToDhis2.js
+++ jobs/2a.addToDhis2.js
@@ -1,4 +1,31 @@
 fn(state => {
   console.log(state.data);
   return state;
-})
+});
+
+createTEI({
+   orgUnit: 'DiszpKrYNg8',
+   trackedEntityType: 'nEenWmSyUEp',
+   attributes: [
+      {
+         attribute: 'zDhUuAYrxNC', // last name
+         value: 'Test Only',
+      },
+      {
+         attribute: 'w75KJ2mc4zz', // first name
+         value: 'Gigiwe',
+      },
+      { attribute: 'P2cwLGskgxn', // phone Number
+        value: dataValue('contact.urn').slice(-10)
+      }
+   ],
+   enrollments: [
+      {
+         orgUnit: 'DiszpKrYNg8',
+         program: 'uy2gU8kT1jF',
+         programState: 'oRySG82BKE6', // active
+        // enrollmentDate: '2021-01-04',
+        // incidentDate: '2021-01-04',
+      },
+   ],
+});
