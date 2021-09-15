f3e5903ed68ba75d311f676dcadac9bebaf784d9
Taylor Downs <taylor@openfn.org>
2021-09-15 20:57:47 +0100

    taylor@openfn.org's change on OpenFn.org

--- jobs/2a.addToDhis2.js
+++ jobs/2a.addToDhis2.js
@@ -1,31 +1,34 @@
 fn(state => {
-  console.log(state.data);
-  return state;
+  const { name, urn } = state.data.contact;
+  
+  const firstName = name ? name.split(' ')[0] : 'Anonymous';
+  const lastName = name ? name.split(' ')[1] : 'Test';
+  const today = new Date();
+  
+  const attributes = [
+    { attribute: 'zDhUuAYrxNC', value: lastName},
+      {
+         attribute: 'w75KJ2mc4zz', // first name
+         value: firstName,
+      },
+      { attribute: 'P2cwLGskgxn', // phone Number
+        value: state.data.contact.urn.slice(-10)
+      }];
+    
+  return { ...state, data: {...state.data, attributes, today }};
 });
 
 createTEI({
    orgUnit: 'DiszpKrYNg8',
    trackedEntityType: 'nEenWmSyUEp',
-   attributes: [
-      {
-         attribute: 'zDhUuAYrxNC', // last name
-         value: 'Test Only',
-      },
-      {
-         attribute: 'w75KJ2mc4zz', // first name
-         value: 'Gigiwe',
-      },
-      { attribute: 'P2cwLGskgxn', // phone Number
-        value: state.data.contact.urn.slice(-10)
-      }
-   ],
+   attributes: dataValue('attributes'),
    enrollments: [
       {
          orgUnit: 'DiszpKrYNg8',
          program: 'uy2gU8kT1jF',
          programState: 'oRySG82BKE6', // active
-        // enrollmentDate: '2021-01-04',
-        // incidentDate: '2021-01-04',
+        enrollmentDate: dataValue('today'),
+        incidentDate: dataValue('today'),
       },
    ],
 });
