74ce665fda5e7d84cb3c756bb393ac1f3bb5181c
Taylor Downs <downs.taylor@gmail.com>
2021-09-15 21:00:35 +0100

    cleanup addToDhis2

--- jobs/2a.addToDhis2.js
+++ jobs/2a.addToDhis2.js
@@ -1,34 +1,30 @@
 fn(state => {
   const { name, urn } = state.data.contact;
-  
+
   const firstName = name ? name.split(' ')[0] : 'Anonymous';
   const lastName = name ? name.split(' ')[1] : 'Test';
   const today = new Date();
-  
+
   const attributes = [
-    { attribute: 'zDhUuAYrxNC', value: lastName},
-      {
-         attribute: 'w75KJ2mc4zz', // first name
-         value: firstName,
-      },
-      { attribute: 'P2cwLGskgxn', // phone Number
-        value: state.data.contact.urn.slice(-10)
-      }];
-    
-  return { ...state, data: {...state.data, attributes, today }};
+    { attribute: 'zDhUuAYrxNC', value: lastName },
+    { attribute: 'w75KJ2mc4zz', value: firstName },
+    { attribute: 'P2cwLGskgxn', value: urn.slice(-10) },
+  ];
+
+  return { ...state, data: { ...state.data, attributes, today } };
 });
 
 createTEI({
-   orgUnit: 'DiszpKrYNg8',
-   trackedEntityType: 'nEenWmSyUEp',
-   attributes: dataValue('attributes'),
-   enrollments: [
-      {
-         orgUnit: 'DiszpKrYNg8',
-         program: 'uy2gU8kT1jF',
-         programState: 'oRySG82BKE6', // active
-        enrollmentDate: dataValue('today'),
-        incidentDate: dataValue('today'),
-      },
-   ],
+  orgUnit: 'DiszpKrYNg8',
+  trackedEntityType: 'nEenWmSyUEp',
+  attributes: dataValue('attributes'),
+  enrollments: [
+    {
+      orgUnit: 'DiszpKrYNg8',
+      program: 'uy2gU8kT1jF',
+      programState: 'oRySG82BKE6', // active
+      enrollmentDate: dataValue('today'),
+      incidentDate: dataValue('today'),
+    },
+  ],
 });
