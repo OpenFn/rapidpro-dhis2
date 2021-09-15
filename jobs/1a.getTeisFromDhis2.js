908a95e3c1c14b134fe896e8169297b018f6e991
Taylor Downs <downs.taylor@gmail.com>
2021-09-15 17:38:39 +0100

    modify in dhis2 to use data mashing helper

--- jobs/1a.getTeisFromDhis2.js
+++ jobs/1a.getTeisFromDhis2.js
@@ -4,3 +4,22 @@ getTEIs({
   program: 'uy2gU8kT1jF',
   skipPaging: true,
 });
+
+// Manipulate the data from dhis2...
+fn(state => {
+  console.log('Build RapidPro contacts from raw dhis2 response...');
+  const rapidProContacts = state.data.trackedEntityInstances
+    .filter(tei => attrVal(tei, 'phone number'))
+    .map(tei => {
+      const firstName = attrVal(tei, 'first name');
+      const lastName = attrVal(tei, 'last name');
+      const phoneNumber = attrVal(tei, 'phone number');
+      const contact = {
+        name: `${firstName} ${lastName}`,
+        urns: [`tel:${phoneNumber}`],
+        fields: { tei_id: tei.trackedEntityInstance },
+      };
+      return { contact };
+    });
+  return { ...state, rapidProContacts, uuids: [] };
+});
--- jobs/1b.initiateRapidFlow.js
+++ jobs/1b.initiateRapidFlow.js
@@ -1,42 +1,11 @@
-// Manipulate the data from dhis2...
-fn(state => {
-
-  // TODO: move this to the dhis2 adaptor ======================================
-  function valByName(attributes, name) {
-    const elem = attributes.find(a => a.displayName.toLowerCase() == name.toLowerCase())
-    return elem ? elem.value : null
-  }
-  // ==========================================================================
-
-  console.log('Build RapidPro contacts from raw dhis2 response...');
-  const rapidProContacts = state.data.trackedEntityInstances
-    .filter(tei => valByName(tei.attributes, 'phone number'))
-    .map(tei => {
-      const firstName = valByName(tei.attributes, 'first name')
-      const lastName = valByName(tei.attributes, 'last name')
-      const phoneNumber = valByName(tei.attributes, 'phone number');
-      const contact = {
-        name: `${firstName} ${lastName}`,
-        urns: [`tel:${phoneNumber}`],
-        fields: { tei_id: tei.trackedEntityInstance }
-      }
-      return { contact };
-    })
-  return { ...state, rapidProContacts, uuids: [] };
-});
-
 // For each item in this rapidProContact array...
 each(
   '$.rapidProContacts[*]',
   // Add contacts to RapidPro...
-  addContact(
-    dataValue('contact'),
-    state => {
-      console.log(state.uuids)
-      state.uuids.push(state.data.uuid)
-      return state;
-    }
-  )
+  upsertContact(dataValue('contact'), state => {
+    state.uuids.push(state.data.uuid);
+    return state;
+  })
 );
 
 // Start communication flows with the contacts...
@@ -44,4 +13,4 @@ startFlow({
   flow: '65e20d7c-2490-4d0e-a96e-b58ee7d0858b',
   contacts: state => state.uuids,
   restart_participants: false,
-})
+});
