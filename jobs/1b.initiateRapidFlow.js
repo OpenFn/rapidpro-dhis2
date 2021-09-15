182c83c418672ac4e1e0ced6b117e596c5664a15
Elias W. BA <Elias W. BA>
2021-09-15 13:39:13 +0000

    get TEI data from DHIS, extract/transform TEIs, add contacts to RapidPro and start flow

--- jobs/1b.initiateRapidFlow.js
+++ jobs/1b.initiateRapidFlow.js
@@ -1,17 +1,34 @@
+// Manipulate the data from dhis2...
 fn(state => {
-  console.log('Manipulate data from dhis2...');
-  return state;
+  console.log('Build RapidPro contacts from raw dhis2 response...');
+  const rapidProContacts = state.data.trackedEntityInstances.map(tei => {
+    const firstName = tei.attributes.find(attribute => attribute.displayName.toLowerCase() == 'first name').value
+    const lastName = tei.attributes.find(attribute => attribute.displayName.toLowerCase() == 'last name').value
+    const contact = {
+      name: `${firstName} ${lastName}`,
+      fields: { tei_id: 'something' }
+    }
+    return { contact };
+  })
+  return { ...state, rapidProContacts, data: { uuids: [] } };
 });
 
-addContact({
-  name: 'Taylor',
-  language: 'ENG',
-  urns: ['tel:+447904413406'],
-});
+// For each item in this rapidProContact array...
+each(
+  '$.rapidProContacts[*]',
+  // Add contacts to RapidPro...
+  addContact(
+    dataValue('contact'),
+    state => {
+      state.data.uuids.push(state.data.uuid)
+      return state;
+    }
+  )
+)
 
+// Start communication flows with the contacts...
 startFlow({
   flow: '65e20d7c-2490-4d0e-a96e-b58ee7d0858b',
-  contacts: [dataValue('uuid')],
-  // contacts: ['4924c72f-720c-46ec-8259-3fbef9efda20'],
+  contacts: dataValue('uuids'),
   restart_participants: false,
-});
+})
